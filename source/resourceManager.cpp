6b675faf9bc70b9f205abd5998a0573a85f579d3
Campbell <cfl0008@uah.edu>
2023-03-09 16:58:46 -0600

    Header Include Edits
    Changed the include header paths for each .h file in resourceManager.cpp to "header/*" instead of "../Course_Scheduling/header/*".
    
    Consider moving all #include files to resourceManager.h?

--- source/resourceManager.cpp
+++ source/resourceManager.cpp
@@ -1,9 +1,9 @@
-#include "../Course_Scheduling/header/course.h"
-#include "../Course_Scheduling/header/department.h"
-#include "../Course_Scheduling/header/engine.h"
-#include "../Course_Scheduling/header/instructor.h"
-#include "../Course_Scheduling/header/room.h"
-#include "../Course_Scheduling/header/mainwindow.h"
+#include "header/course.h"
+#include "header/department.h"
+#include "header/engine.h"
+#include "header/instructor.h"
+#include "header/room.h"
+#include "header/mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
