20af61834b184005cb31ea8851468e8085918ee1
Actual-Heathen <tonyyluis@hotmail.com>
2023-03-12 12:54:56 -0400

    copy Henry code for conflict fix

---
+++ header/course.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "dayTime.h"
+#include <string>
+using namespace std;
+
+class Course {
+private:
+	char sectionType = ' ';
+	int crn = 0;
+	string courseNumber = "";
+	string sectionNumber = "";
+	string title = "";
+	double credit = 0;
+	int maxEnroll = 0;
+	bool conflict = false;
+	DayTime schedule;
+
+public:
+	void setSectionType(char input);
+	void setCRN(int input);
+	void setCourseNumber(string input);
+	void setSectionNumber(string input);
+	void setTitle(string input);
+	void setCredit(double input);
+	void setMaxEnroll(int input);
+	void setConflict(bool input);
+	void setOccupied(int days, int hours, bool status);
+	char getSectionType();
+	int getCRN();
+	string getCourseNumber();
+	string getSectionNumber();
+	string getTitle();
+	double getCredit();
+	int getMaxEnroll();
+	bool getConflict();
+	bool getOccupied(int days, int hours);
+};
---
+++ header/room.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "dayTime.h"
+#include <string>
+using namespace std;
+
+class RoomInfo {
+private:
+	string buildingName;
+	string roomNumber;
+	int maxCapacity;
+	bool conflict = false;
+	DayTime schedule;
+
+public:
+	void setBuildingName(string input);
+	void setRoomNumber(string input);
+	void setCapacity(int input);
+	void setConflict(bool input);
+	void setOccupied(int days, int hours, bool status);
+	string getBuildingName();
+	string getRoomNumber();
+	int getCapacity();
+	bool getConflict();
+	bool getOccupied(int days, int hours);
+};
